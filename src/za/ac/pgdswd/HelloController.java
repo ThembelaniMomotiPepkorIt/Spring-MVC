046252e1a8235e29bd4cc5bebd93ec00cc253301
Thembelani <lonwabo.momoti@gmail.com>
2015-09-08 22:44:11 +0200

    ep9 Not running

--- src/za/ac/pgdswd/HelloController.java
+++ src/za/ac/pgdswd/HelloController.java
@@ -1,30 +1,23 @@
 package za.ac.pgdswd;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/greeting")
+@RequestMapping("greeting")
 public class HelloController {
 
-	@RequestMapping("/welcome")
-	public ModelAndView helloWorldEng() {
+	@RequestMapping("/welcome/{country}/{lang}")
+	public ModelAndView helloWorldEng(@PathVariable Map<String, String> pathVar) {
 		ModelAndView modelAndView = new ModelAndView("HelloPage");
 		modelAndView.addObject("welcomeGreeting",
-				"Hello,This is Thembelani's project");
+				"Hello,This is live from "  + pathVar.get("country")+ " in "+pathVar.get("lang"));
 
 		return modelAndView;
 	}
 	
-
-	@RequestMapping("/wamkelekile")
-	public ModelAndView helloWorldXh() {
-		ModelAndView modelAndView = new ModelAndView("HelloPage");
-		modelAndView.addObject("welcomeGreeting",
-				"Hello,This is Thembelani's project");
-
-		return modelAndView;
-	}
-
 }
