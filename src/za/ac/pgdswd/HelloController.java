ea0feb06487e7070c9226a2faa34d52c6ef0753a
Thembelani <lonwabo.momoti@gmail.com>
2015-09-08 21:41:26 +0200

    End ep8 Error when methods with same name, not running

--- src/za/ac/pgdswd/HelloController.java
+++ src/za/ac/pgdswd/HelloController.java
@@ -5,14 +5,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/greeting")
 public class HelloController {
 
 	@RequestMapping("/welcome")
-	public ModelAndView helloWorld() {
+	public ModelAndView helloWorldEng() {
 		ModelAndView modelAndView = new ModelAndView("HelloPage");
 		modelAndView.addObject("welcomeGreeting",
 				"Hello,This is Thembelani's project");
 
 		return modelAndView;
 	}
+	
+
+	@RequestMapping("/wamkelekile")
+	public ModelAndView helloWorldXh() {
+		ModelAndView modelAndView = new ModelAndView("HelloPage");
+		modelAndView.addObject("welcomeGreeting",
+				"Hello,This is Thembelani's project");
+
+		return modelAndView;
+	}
+
 }
