092c2957073893fc02590b549dc87c1ffce03ec5
Thembelani <lonwabo.momoti@gmail.com>
2015-09-10 21:52:40 +0200

    ep11 Error with mapping, probably server problem

--- src/za/ac/pgdswd/controllers/StudentAdmissionController.java
+++ src/za/ac/pgdswd/controllers/StudentAdmissionController.java
@@ -1,14 +1,17 @@
 package za.ac.pgdswd.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import za.ac.pgdswd.models.Student;
+
 @Controller
 public class StudentAdmissionController {
-  
+
 	@RequestMapping(value="/admissionForm.html", method=RequestMethod.GET)
 	public ModelAndView getAdmissionFrom(){
 		
@@ -17,10 +20,18 @@ public class StudentAdmissionController {
 		return modelAndView;
 	}
 	
-	@RequestMapping(value="/submitAdmissionForm.html", method=RequestMethod.POST)
+	/*@RequestMapping(value="/submitAdmissionForm.html", method=RequestMethod.POST)
 	public ModelAndView submitAdmissionForm(@RequestParam(value="studentName", defaultValue="anonymous")String name, @RequestParam("hobby") String hobby){
+		ModelAndView modelAndView = new ModelAndView("AdmissionSuccess");
+		modelAndView.addObject("msg", "Details submitted to you: "+name+" Hobby: "+hobby);
+		return null;
+	}*/
+
+	@RequestMapping(value="/submitAdmissionForm.html", method=RequestMethod.POST)
+	public ModelAndView submitAdmissionForm(@ModelAttribute("student1") Student student1){
 		ModelAndView modelAndView = new ModelAndView("AdmissionSuccess");
 		modelAndView.addObject("msg", "Details submitted to you: "+name+" Hobby: "+hobby);
 		return null;
 	}
 }
+
