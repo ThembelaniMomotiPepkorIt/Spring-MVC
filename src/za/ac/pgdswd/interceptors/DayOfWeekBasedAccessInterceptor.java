a84f06a48519e3fbdbe44f18b081b3299a5d7276
Thembelani <lonwabo.momoti@gmail.com>
2015-09-15 21:33:16 +0200

    ep24 PostHandle and AfterCompletion...not running

--- src/za/ac/pgdswd/interceptors/DayOfWeekBasedAccessInterceptor.java
+++ src/za/ac/pgdswd/interceptors/DayOfWeekBasedAccessInterceptor.java
@@ -5,19 +5,35 @@ import java.util.Calendar;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class DayOfWeekBasedAccessInterceptor extends HandlerInterceptorAdapter{
-	boolean flag = true;
-	public boolean preHandle(HttpServletRequest request,
-							HttpServletResponse response, 
-							Object handler) throws Exception{
-		
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+		boolean flag = true;
 		int dayOfWeek = Calendar.DAY_OF_WEEK;
+		
 		if (dayOfWeek ==1){
 			response.getWriter().write("The website can is not  accessible on Sunday");
 			flag = false;
 		}
 		return flag;
 	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, 
+							Object handler, ModelAndView modelAndView) throws Exception{
+		
+		System.out.println("Called post handle for "+request.getRequestURI().toString());
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, 
+								Object handler, Exception ex) throws Exception {
+		
+		System.out.println("Called after completion for "+request.getRequestURI().toString());
+		
+	}
+	
 }
